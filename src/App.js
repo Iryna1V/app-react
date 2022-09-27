3d3981f9e6e4c16bf08982711bd05c3bdad2e504
Iryna1V <ivhaievska@gmail.com>
2022-09-27 18:49:41 +0200

    axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
